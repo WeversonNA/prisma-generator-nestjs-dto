458ada739e618e21bb98f026d45587ac6afddbb0
WeversonNA <98351856+WeversonNA@users.noreply.github.com>
2025-06-25 11:33:46 -0300

    fix: FieldType enum cannot correct create (#12)
    Co-authored-by: weverson.almeida <weverson.almeida@globalsys.com.br>

--- src/generators/helpers/array-helper.ts
+++
@@ -1,9 +0,0 @@
-export class ArrayHelper {
-  static uniq<T>(input: T[]): T[] {
-    return Array.from(new Set(input));
-  }
-
-  static concatIntoArray<T>(source: T[], target: T[]): void {
-    source.forEach((item) => target.push(item));
-  }
-}
